119692f2e0d0827b731134e660013ddae84e607a
hannah413 <manningh@ohsu.edu>
2018-08-17 13:05:42 -0700

    Commenting out vestigial expr lines

--- aracne_scripts/preprocess.py
+++ aracne_scripts/preprocess.py
@@ -1,6 +1,7 @@
 """
-
-hardcoded for brca
+Minimal preprocessing currently takes place. This step only consists of generating a mapped regulator file
+for input into aracne. It also outputs the expression file in a now-tab-separated and non gzipped format for
+ARACNE, although this may not be necessary.
 
 
 Reads in
@@ -8,8 +9,11 @@ Reads in
     (2) regulator list (hugo IDs)
 
 Spits out
-    (1) unzipped and properly formatted ensgac tatlow tcga expr file
-    (2) regulator list (ensgac IDs)
+    (1) unzipped and properly formatted ensembl tatlow tcga expr file
+    (2) regulator list (ensembl IDs)
+
+Author: Hannah Manning
+Date: April 2018
 
 """
 
@@ -18,7 +22,11 @@ import numpy as np
 import argparse
 
 def map_regulators(regulatorin, regulatorout, mapfile):
-    mapdf = pd.read_csv(mapfile, header=0, usecols=['V2','V5','V6'], sep=" ")
+    """Takes a regulator input file with HUGO IDs (one per line) and maps them to ensembl IDs.
+    Generates a new regulator file with ensembl IDs.
+    """
+
+    mapdf = pd.read_csv(mapfile, header=0, usecols=['V2', 'V5', 'V6'], sep=" ")
     mapdf = mapdf.set_index('V2')
 
     with open(regulatorin, "r") as fh:
@@ -43,16 +51,16 @@ def map_regulators(regulatorin, regulatorout, mapfile):
 def main():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("-ei", "--expression_in",
-                        type=str,
-                        help='Full path to expression file (Kallisto-processed by Tatlow et al.\n'
-                             'i.e. path/to/TCGA_BRCA_bygene.txt.gz; (can be gzipped or not)'
-                        )
+    #parser.add_argument("-ei", "--expression_in",
+    #                    type=str,
+    #                    help='Full path to expression file (Kallisto-processed by Tatlow et al.\n'
+    #                         'i.e. path/to/TCGA_BRCA_bygene.txt.gz; (can be gzipped or not)'
+    #                    )
 
-    parser.add_argument("-eo", "--expression_out",
-                        type=str,
-                        help='Full path to new expression file (filtered, mapped).'
-                        )
+    #parser.add_argument("-eo", "--expression_out",
+    #                    type=str,
+    #                    help='Full path to new expression file (filtered, mapped).'
+    #                    )
 
     parser.add_argument("-ro", "--regulator_out",
                         type=str,
@@ -61,21 +69,24 @@ def main():
 
     args = parser.parse_args()
 
-    exprin = args.expression_in
-    exprout = args.expression_out
+    #exprin = args.expression_in
+    #exprout = args.expression_out
     regulatorout = args.regulator_out
 
     lustrehome = '/home/exacloud/lustre1/CompBio/manningh/'
-    regulatorin = lustrehome + 'Data/tf_act/sens_val/pancancer-tfgenes_2014_04_17_filtered.txt'
+    #regulatorin = lustrehome + 'Data/tf_act/sens_val/pancancer-tfgenes_2014_04_17_filtered.txt'
+    regulatorin = lustrehome + 'Data/tf_act/sens_val/PC_regulators.txt'
     mapfile = lustrehome + 'PyCharmProjects/smmartVIPER/data/target_id.txt'
 
     # todo: add compression checker
-    expr = pd.read_csv(exprin, compression='gzip', sep=" ", header=0, index_col=1)
-    expr = expr.drop('Unnamed: 0', 1)
+    #expr = pd.read_csv(exprin, compression='gzip', sep=" ", header=0, index_col=1)
+    #expr = expr.drop('Unnamed: 0', 1)
 
     map_regulators(regulatorin, regulatorout, mapfile)
 
-    expr.to_csv(exprout, sep='\t')
+    # todo: this step is not currently necessary, but may have to write out expr file like this if other
+    # todo: preprocessing occurs
+    #expr.to_csv(exprout, sep='\t')
 
 
 if __name__ == '__main__':
